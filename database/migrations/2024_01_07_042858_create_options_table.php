95921a4c787b1446a470b3d052b3a874d5097ebb
tony blake <thanhle09t2bkdn@gmail.com>
2024-01-07 11:38:41 +0700

    quizzes, options

---
+++ database/migrations/2024_01_07_042858_create_options_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('options', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('quiz_id');
+            $table->string('name')->nullable();
+            $table->boolean('is_answer')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('options');
+    }
+}
---
+++ database/migrations/2024_01_07_043610_fk_quizzes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FkQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('quizzes', function (Blueprint $table) {
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('audios', function (Blueprint $table) {
+            $table->dropForeign('quizzes_post_id_foreign');
+        });
+    }
+}

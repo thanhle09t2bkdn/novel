0e02421902de04c1b75e67e634179bfb453da54b
tony blake <thanhle09t2bkdn@gmail.com>
2024-01-09 17:08:29 +0700

    remove

--- routes/web/frontend/public.php
+++ routes/web/frontend/public.php
@@ -1,10 +1,3 @@
 <?php
 
 Route::get('', 'PublicController@index')->name('public.index');
-Route::get('chuyen-de', 'PublicController@article')->name('public.article');
-Route::get('sach', 'PublicController@book')->name('public.book');
-Route::get('chu-de/{slug}', 'PublicController@topic')->name('public.topic');
-Route::get('{slug}', 'PublicController@single')->name('public.single');
-Route::get('quiz/{slug}', 'PublicController@quiz')->name('public.quiz');
-Route::get('audio/{slug}', 'PublicController@audio')->name('public.audio');
-Route::get('chuyen-muc/{slug}', 'PublicController@postAudio')->name('public.postAudio');

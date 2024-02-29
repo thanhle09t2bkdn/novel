3002bf01594ec08ef2b48889697e27b837cecad2
tony blake <thanhle09t2bkdn@gmail.com>
2024-02-29 22:28:00 +0700

    header

--- resources/views/frontend/public/svg.blade.php
+++ resources/views/frontend/public/svg.blade.php
@@ -15,7 +15,7 @@
                         <h1>{{ $post->name }}</h1>
                         <div class="story-detail__bottom mb-3">
                             <div class="row">
-                                <div class="col-12 col-md-12 col-lg-3">
+                                <div class="col-12">
                                     <div class="rating-container">
                                         <div class="my-rating" data-rating="{{ $post->rate }}"></div>
                                         <p><strong>{{ $post->view_number }}</strong> Views</p>

b0a02947435fb64f7ba4955d1162405f38a5bc8a
tony blake <thanhle09t2bkdn@gmail.com>
2024-03-01 14:32:55 +0700

    crawl

--- app/Console/Commands/NovelCoolChapterDetailCommand.php
+++ app/Console/Commands/NovelCoolChapterDetailCommand.php
@@ -52,26 +52,30 @@ class NovelCoolChapterDetailCommand extends Command
     {
 
         Log::info('NovelCoolCommandSTART:');
-        $chapters = $this->chapterRepository
-            ->where('content', null, '=')
-            ->orderBy('name', 'asc')->get();
-        foreach ($chapters as $chapter) {
-            Log::info('NovelCoolCommandEND: post detail ' . $chapter->name . PHP_EOL);
-            try {
-                $content = Http::withHeaders([
-                    'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-                ])->get($chapter->link);
-                $dom = HtmlDomParser::str_get_html($content->body());
+        do {
+            $chapters = $this->chapterRepository
+                ->where('content', null, '=')
+                ->orderBy('name', 'asc')
+                ->paginate(200);
+            foreach ($chapters as $chapter) {
+                Log::info('NovelCoolCommandEND: post detail ' . $chapter->name . PHP_EOL);
+                try {
+                    $content = Http::withHeaders([
+                        'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+                    ])->get($chapter->link);
+                    $dom = HtmlDomParser::str_get_html($content->body());
 
-                $contentObject = $dom->find('.overflow-hidden', 0);
-                $replaceTitle = str_replace($chapter->name, '', $contentObject->text());
-                $replaceReport = str_replace('Chapter end   Report', '', $replaceTitle);
-                $chapter->content = preg_replace("/[\r\n]/", "<p>", $replaceReport);
-                $chapter->save();
-            } catch (\Exception $e) {
-                Log::error('Error:', [$e->getMessage()]);
+                    $contentObject = $dom->find('.overflow-hidden', 0);
+                    $replaceTitle = str_replace($chapter->name, '', $contentObject->text());
+                    $replaceReport = str_replace('Chapter end   Report', '', $replaceTitle);
+                    $chapter->content = preg_replace("/[\r\n]/", "<p>", $replaceReport);
+                    $chapter->save();
+                } catch (\Exception $e) {
+                    Log::error('Error:', [$e->getMessage()]);
+                }
             }
-        }
+        } while (count($chapters));
+
         Log::info('NovelCoolCommandEND:');
         return 0;
     }

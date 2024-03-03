7e4c2c4a58581d5db486d7f76bf7aa8ac99e7aa5
tony blake <thanhle09t2bkdn@gmail.com>
2024-03-03 11:50:50 +0700

    remote name

--- app/Console/Commands/NovelCoolChapterCommand.php
+++ app/Console/Commands/NovelCoolChapterCommand.php
@@ -59,7 +59,6 @@ class NovelCoolChapterCommand extends Command
                 ->orderBy('name')
                 ->paginate(200);
             foreach ($posts as $post) {
-                Log::info('NovelCoolCommandEND: post ' . $post->link . PHP_EOL);
                 try {
                     $content = Http::withHeaders([
                         'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',

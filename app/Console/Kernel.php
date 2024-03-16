8febf914547b29fd331dd61b8660451e6c558293
tony blake <thanhle09t2bkdn@gmail.com>
2024-03-16 10:24:14 +0700

    command:royal-road-update-latest-chapter

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('command:novel-cool-update-latest-chapter')->hourly();
+         $schedule->command('command:royal-road-update-latest-chapter')->hourly();
     }
 
     /**
